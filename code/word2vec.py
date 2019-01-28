c55a2c423783960a088162eeb29ffd805a6140c1
Quentin Meeus <qmeeus@gmail.com>
2019-01-28 02:17:28 +0100

    migration to py3 + fix file does not exist error

--- code/word2vec.py
+++ code/word2vec.py
@@ -1,26 +1,28 @@
+from __future__ import print_function
 import gensim
-import codecs
+import os
+# import codecs
 
 class MySentences(object):
     def __init__(self, filename):
         self.filename = filename
 
     def __iter__(self):
-        for line in codecs.open(self.filename, 'r', 'utf-8'):
-            yield line.split()
+        with open(self.filename) as f:
+            for line in f.readlines():
+                yield line.split()
 
 
 def main(domain):
-    source = '../preprocessed_data/%s/train.txt' % (domain)
-    model_file = '../preprocessed_data/%s/w2v_embedding' % (domain)
+    source = '../datasets/%s/train.txt' % (domain)
+    output_dir = "../preprocessed_data/%s" % (domain)
+    os.makedirs(output_dir, exist_ok=True)
+    model_file = output_dir + '/w2v_embedding'
     sentences = MySentences(source)
     model = gensim.models.Word2Vec(sentences, size=200, window=5, min_count=10, workers=4)
     model.save(model_file)
 
 
-print 'Pre-training word embeddings ...'
+print('Pre-training word embeddings ...')
 main('restaurant')
 main('beer')
-
-
-
